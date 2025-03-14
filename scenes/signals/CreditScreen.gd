e9d231dac3e14237e5c0ba68c381b97a0824ec4b
Joselin Permata Aprillia <103546691+joselinprmt@users.noreply.github.com>
2025-03-14 16:14:04 +0700

    fix(main): fix bug lives not resetting to 3 after credit scene, adjust connected method _on_new_game_pressed

--- scenes/signals/CreditScreen.gd
+++ scenes/signals/CreditScreen.gd
@@ -1,15 +1,8 @@
 extends Sprite2D
 
-@export var scene_name: String = "Level1"
-
-var can_restart := false
+@onready var link_button: LinkButton = $LinkButton
 
 
 func _ready() -> void:
+	Global.lives = 3
 	await get_tree().create_timer(4).timeout
-	can_restart = true
-
-
-func _process(_delta) -> void:
-	if can_restart and Input.is_action_just_pressed("restart"):
-		get_tree().change_scene_to_file("res://scenes/" + scene_name + ".tscn")
